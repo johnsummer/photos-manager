353cbc52fac05c645cfd075e3455908cd7cce97a
john_summser <john.midsummer.ac@gmail.com>
2020-06-06 18:19:36 +0900

    feat: 子フォルダを同期するメソッドを追加

--- src/backup-photos.py
+++ src/backup-photos.py
@@ -3,6 +3,7 @@
 import subprocess
 import sys
 import chardet
+import os
 
 def BackupOneFolder(sourceFolder, destinationFolder):
 
@@ -16,6 +17,36 @@ def BackupOneFolder(sourceFolder, destinationFolder):
 
     print(res.stdout.decode(chardet.detect(res.stdout)["encoding"]))
 
+def SynchronizeFolders(sourceParentFolder, destinationParentFolder):
+
+    # List sub folders of sourceParentFolder and destinationParentFolder
+    sourceSubFolders = os.listdir(sourceParentFolder)
+    destinationSubFolders = os.listdir(destinationParentFolder)
+
+    # Find the pair of folders having same name from source list and destinaition list of sub folders
+    # and synchronize
+
+    sourceSubFolders.sort()
+    destinationSubFolders.sort()
+
+    indexDestSubFolder = 0
+
+    for sourceSubFolder in sourceSubFolders:
+        if sourceSubFolder < destinationSubFolders[indexDestSubFolder]:
+            continue
+        elif sourceSubFolder == destinationSubFolders[indexDestSubFolder]:
+            BackupOneFolder(sourceSubFolder, destinationSubFolders[indexDestSubFolder])
+        else:
+            while(sourceSubFolder > destinationSubFolders[indexDestSubFolder]):
+                indexDestSubFolder = indexDestSubFolder + 1
+            if indexDestSubFolder >= len(destinationSubFolders):
+                break
+            if sourceSubFolder == destinationSubFolders[indexDestSubFolder]:
+                BackupOneFolder(sourceSubFolder, destinationSubFolders[indexDestSubFolder])
+
+
+# For test
 if __name__ == "__main__":
 
-    BackupOneFolder(str(sys.argv[1]), str(sys.argv[2]))
+    # BackupOneFolder(str(sys.argv[1]), str(sys.argv[2]))
+    SynchronizeFolders(str(sys.argv[1]), str(sys.argv[2]))
